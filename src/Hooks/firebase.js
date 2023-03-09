2c564164c7e729f7fa08a0e3abe3b7d7353a644d
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-09 15:47:52 -0800

    Added Firebase/Storage

--- src/Hooks/firebase.js
+++ src/Hooks/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import {getDatabase} from 'firebase/database'
-import {getStorage} from 'firebase/storage'
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN ,
   projectId:"practicetrelo2",
-  storageBucket:process.env.REACT_APP_STORAGE_BUCKET,
+  storageBucket: "practicetrelo2.appspot.com",
   messagingSenderId: process.env.REACT_APP_MESSAGE_SENDIND_ID,
   appId:process.env.REACT_APP_APP_ID,
 };
@@ -15,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app)
 export const storage = getStorage(app)
+
 
