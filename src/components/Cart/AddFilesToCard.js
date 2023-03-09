c1d9af0f19b677153f6df68dcbdc8bce65cceca6
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-09 15:46:25 -0800

    Added Button for Add Img

--- src/components/Cart/AddFilesToCard.js
+++ src/components/Cart/AddFilesToCard.js
@@ -1,11 +1,30 @@
-const AddFilesToCard = () => {
+import React, { useState } from "react"
+import { storage } from "../../Hooks/firebase"
+import { ref, uploadBytes } from "firebase/storage"
+import uuid from "react-uuid"
+const AddFilesToCard = (props) => {
+  const [imageUpload, setImageUpload] = useState(null)
+  const [imageUploaded, setImageUploaded]= useState(false)
+  const [uid, setUid] = useState('')
+  const UploadFiles = (e) => {
+    e.preventDefault()
+    if (imageUpload === null) return
+    const imageRef = ref(storage, `images/${uid}/${imageUpload.name}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      setImageUploaded(true)
+    })
+  }
+  props.sendUid({
+    uid,
+    imageUploaded,
+  })
+  
+  return (
+    <div>
+      <input onChange={(event => { setImageUpload(event.target.files[0], setUid(uuid())) })} type="file" />
+      <button onClick={UploadFiles}>Upload</button>
+    </div>
+  )
 
-    return(
-        <>
-      <input  type="file"/>
-     
-      </>
-    )
-    
 }
 export default AddFilesToCard
