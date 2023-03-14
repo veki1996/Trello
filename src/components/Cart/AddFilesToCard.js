0cf762dea336debf5e40683c4224302fa4b6cd6f
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-14 16:14:39 -0700

    no message

--- src/components/Cart/AddFilesToCard.js
+++ src/components/Cart/AddFilesToCard.js
@@ -2,32 +2,28 @@ import React, { useState } from "react"
 import { storage } from "../../Hooks/firebase"
 import { ref, uploadBytes } from "firebase/storage"
 import './AdFilesToCard.css';
-import uuid from "react-uuid"
 import Upload from '../Imeges/upload.png'
 const AddFilesToCard = (props) => {
   const [imageUpload, setImageUpload] = useState(null)
   const [imageUploaded, setImageUploaded] = useState(false)
-  const [uid, setUid] = useState('')
+
   const UploadFiles = (e) => {
     e.preventDefault()
     if (imageUpload === null) return
-    const imageRef = ref(storage, `images/${uid}/${imageUpload.name}`);
+    const imageRef = ref(storage, `images/${props.uids}/${imageUpload.name}`);
     uploadBytes(imageRef, imageUpload).then(() => {
       setImageUploaded(true)
     })
   }
-  props.sendUid({
-    uid,
-    imageUploaded,
-  })
+ 
 
   return (
-    <div className="MainDiv">
+    <div className="n">
       <label>
-        <input onChange={(event => { setImageUpload(event.target.files[0], setUid(uuid())) })} type="file" />
+        <input onChange={(event => { setImageUpload(event.target.files[0],) })} type="file" />
         <img src={Upload} />
       </label>
-      <button onClick={UploadFiles}>{!imageUpload ? 'Upload' : 'Uploaded'}</button>
+      <button onClick={UploadFiles}>{!imageUploaded ? 'Upload' : 'Uploaded'}</button>
     </div>
   )
 
