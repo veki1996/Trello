183a445e942ac21a07cfddfdfdf2d418dd0db5c4
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-23 11:34:22 -0700

    Updated Uuid of Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,38 @@
 import Clasess from './Card.module.css'
 import { db } from '../Hooks/firebase'
-import { set, ref, onValue } from 'firebase/database'
-import { useState, useEffect } from 'react'
+import { ref, onValue } from 'firebase/database'
+import { useState, useEffect, useContext } from 'react'
 import CardPage from './CardPage'
+import CartCtxTF from './Store/auth-context'
 
 const Card = (props) => {
     const [todos, setTodos] = useState([])
+    const Ctx = useContext(CartCtxTF)
+    const UserID = Ctx.RegisterUuid
+   
+   
     useEffect(() => {
         onValue(ref(db), (snapshot) => {
             setTodos([])
             const data = snapshot.val()
             if (data !== null) {
-                Object.values(data).map((todo) => {
-                    setTodos((oldArray) => [...oldArray, todo])
-                })
+                    Object.values(data[UserID]).map((todo) => {
+                        setTodos((oldArray) => [...oldArray, todo])
+                    })
+                
             }
         })
     }, [])
-    const aray = todos.map(todo =>  <CardPage openCard={props.openCard} OpenModal={props.OpenModal} todo={todo} key={todo.todo} name={todo.Names} column={todo.todo}/>)
     
+    const aray = todos.map(todo => <CardPage
+
+        openCard={props.openCard}
+        OpenModal={props.OpenModal}
+        todo={todo}
+        key={todo.todo}
+        name={todo.Names}
+        column={todo.todo}
+    />)
     return (
         <div className={Clasess.Column}>
             {aray}
