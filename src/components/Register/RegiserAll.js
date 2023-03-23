6fec15929a4637a365188627ae959c3dd3a2b3e6
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-23 14:40:33 -0700

    no message

--- src/components/Register/RegiserAll.js
+++ src/components/Register/RegiserAll.js
@@ -3,7 +3,6 @@ import Register from "./Register"
 import Login from "./Login"
 const RegisterAll = (props) => {
     const [Logged, setLogged] = useState(false)
-    console.log(Logged)
     return (
         <>
             {!Logged && <Register LoggedIn={props.LoggedIn} LoginBtn={(val) => { setLogged(val) }} />}
