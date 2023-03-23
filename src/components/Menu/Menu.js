fc6fedcae4e2057d6c4bf43424f67044b8f16cdf
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-23 11:41:10 -0700

    added logout btn in menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,7 +2,8 @@ import React from "react";
 import "./HorizontalMenu.css";
 import { useState, useContext } from "react";
 import CartCtxTF from "../Store/auth-context";
-const HorizontalMenu = () => {
+import LogoutPage from "../ButtonsAndInputs/LogoutPage";
+const HorizontalMenu = (props) => {
     const ctx = useContext(CartCtxTF)
     const [input, setInput] = useState(true)
     const ShowBiggerInput = () => {
@@ -22,9 +23,11 @@ const HorizontalMenu = () => {
                 <div className="menu-item">Starrted</div>
                 <div className="menu-item">Templates</div>
                 <div className="menu-item">Create</div>
+                <button onClick={()=>{props.sendModal(true)}}>Add Column</button>
             </div>
             <div>
                 {input ? <input onClick={ShowBiggerInput} placeholder="Search" /> : <input placeholder="Search" onChange={InputValue} className="ExpendInput" onBlur={onBlurInput} />}
+                <LogoutPage/>
             </div>
         </div>
     );
