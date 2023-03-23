0e698105d0e068c55446df30a0b4b275c8d147af
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-23 11:35:54 -0700

    removed and updated css

--- src/Hooks/firebase.js
+++ src/Hooks/firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from "firebase/app";
 import {getDatabase} from 'firebase/database'
 import { getStorage } from "firebase/storage";
-
+import {getAuth} from 'firebase/auth'
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API,
+  apiKey: "AIzaSyALuEJMTVt7vHOxaB3kncWDM4y5FPCnaqg",
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN ,
   projectId:"practicetrelo2",
   storageBucket: "practicetrelo2.appspot.com",
@@ -14,6 +14,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app)
+export const auth = getAuth(app)
 export const storage = getStorage(app)
+
 
 
