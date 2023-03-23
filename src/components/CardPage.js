7f912111a3b0218efd012c5ae899d84f57d7bb7a
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-23 11:34:45 -0700

    remove Search filter

--- src/components/CardPage.js
+++ src/components/CardPage.js
@@ -10,8 +10,8 @@ const CardPage = (props) => {
     const UpdateCtx = useContext(CartCtxTF)
     const CartCtx = useContext(CartContext)
 
-    const isSearching = UpdateCtx.isSearch
-    const SearchingValue = UpdateCtx.SearchingValues
+   // const isSearching = UpdateCtx.isSearch
+   // const SearchingValue = UpdateCtx.SearchingValues
     const Update = () => {
         CartCtx.sendValue({
             value: props.todo.todo,
@@ -26,24 +26,12 @@ const CardPage = (props) => {
         })
         props.openCard(true)
     }
-
     const names = props.name ? Object.values(props.name) : [];
     let names2 = names.map((item) => <Redovi lines={item.name}  title={item.title} key={item.uuid} columnUUid={props.todo.uuid} uuid={item.uuid} />)
-    const niz = [
-        props.column
-    ]
-    const Filtered = niz.filter(item =>
-        item.toLowerCase().includes(SearchingValue.toLowerCase())
-    );
-    const filterh1 = isSearching ? Filtered : props.column
-
-
-   
-
     return (
         <div className={Clasess.MainPage}>
             <div className={Clasess.MainCardPageSecond}>
-                <h1>{filterh1}</h1>
+                <h1>{props.column}</h1>
                 <div className={Clasess.columnBtns}>
                     <button className={Clasess.EditColumnBtn} onClick={() => Update(props.todo)}>+Edit Column</button>
                     <DelleteBtn todo={props.todo} />
