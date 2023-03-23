645f78959c49ee480c41616e8f75b819849fa154
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-23 11:43:20 -0700

    description and edit

--- src/components/ButtonsAndInputs/ValueDescriprion.js
+++ src/components/ButtonsAndInputs/ValueDescriprion.js
@@ -2,17 +2,10 @@ import React, { useState, useEffect } from "react"
 import Clasess from './ValueDescription.module.css'
 const ValueDescription = (props) => {
     let descriptionValue = props.ValueDes
-    descriptionValue.toString()
-    console.log(typeof(descriptionValue))
-        const des = document.getElementById('Description');
-        if (des) {
-            des.innerHTML = descriptionValue;
-        };
-
-    return (
-        <div id="Description">
-            {props.ValueDes}
-        </div>
-    )
+   
+const OpenDescription=()=>{
+    props.OpenDes(true)
+}
+    return (  <div onClick={OpenDescription} className={Clasess.Main} dangerouslySetInnerHTML={{__html:descriptionValue}}/> )
 }
 export default ValueDescription
