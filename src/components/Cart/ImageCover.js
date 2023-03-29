28a57a5fc423f501678fd1e7cef3e4c9e07dcff1
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-03-29 05:46:58 -0700

    Initial commit

---
+++ src/components/Cart/ImageCover.js
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import { ref,  getDownloadURL,listAll } from "firebase/storage";
+import { storage } from "../../Hooks/firebase";
+import Classes from './ImageCover.module.css'
+const ImageCover=(props)=>{
+    const [imageUrl, setImagesUrl]= useState([])
+    useEffect(() => {
+        const imageListRef = ref(storage, `images/${props.ImageUUid}`)
+        listAll(imageListRef).then((response) => {
+            response.items.forEach((item) => {
+                getDownloadURL(item).then((url) => {
+                    setImagesUrl((prev) => [...prev, url])
+                })
+            })
+        })
+    }, []);
+ console.log(imageUrl)
+    return(
+        <img className={Classes.CoverImage} src={imageUrl}/>
+    )
+}
+export default ImageCover
