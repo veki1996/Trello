81c0a061050b0bdd4c0c4a58fb371d44f2a3996c
Bojan Vecernovic <bojan.vecernovic@gmail.com>
2023-04-06 10:42:27 -0700

    Add Routes an Route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import CartProvider from "./components/Store/CartProvider";
 import RegisterAndPage from "./components/RegisterToPage/RegisterAndPage";
-import RegisterAll from "./components/RegisterToPage/Register/RegiserAll";
-import React, { useContext, useEffect, useState } from "react";
-import CartCtxTF from "./components/Store/auth-context";
+import React, { useEffect, useState } from "react";
 import { auth } from "./Firebase/firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Register/Login";
+import Register from "./pages/Register/Register";
 
 function App() {
-  const Ctx = useContext(CartCtxTF);
-  const login = Ctx.pageLogin;
-
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
@@ -24,11 +21,20 @@ function App() {
     return <div>Loading...</div>;
   }
   return (
+
     <CartProvider>
       <div className="app">
-        {login && user ? <RegisterAndPage /> : <RegisterAll  />}
+        <Routes>
+          {user && <Route path="/*" element={<RegisterAndPage />} />}
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+
+        </Routes>
+
+
       </div>
     </CartProvider>
+
   );
 }
 
